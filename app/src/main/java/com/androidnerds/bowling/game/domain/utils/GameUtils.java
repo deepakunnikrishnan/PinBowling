bc065640fddf790c826d8f4d2144ed70bd55c4a6
Deepak Unnikrishnan <deepak.unnikrishnan@ondotsystems.com>
2021-01-30 20:58:00 -0500

    Utility class for the game.

---
+++ app/src/main/java/com/androidnerds/bowling/game/domain/utils/GameUtils.java
@@ -0,0 +1,39 @@
+package com.androidnerds.bowling.game.domain.utils;
+
+import com.androidnerds.bowling.game.domain.constants.GameConstants;
+
+public final class GameUtils {
+
+
+    private GameUtils() {
+        //empty constructor
+    }
+
+    public static boolean isValidPoint(int currentPoints) {
+        return currentPoints >= 0 && currentPoints <= GameConstants.MAX_POINTS_FOR_ROLL;
+    }
+
+    public static boolean isValidPointWithPreviousPointAs(int currentPoints, int previousPoints) {
+        return isValidPoint(currentPoints) &&
+                isValidPoint(previousPoints) &&
+                currentPoints + previousPoints <= GameConstants.MAX_POINTS_FOR_ROLL;
+    }
+
+    public static boolean isStrike(int points) {
+        return points == GameConstants.MAX_POINTS_FOR_ROLL;
+    }
+
+    public static boolean isSpare(int currentPoints, int previousPoints) {
+        return isValidPoint(currentPoints) &&
+                isValidPoint(previousPoints) &&
+                currentPoints + previousPoints == GameConstants.MAX_POINTS_FOR_ROLL;
+    }
+
+    public static boolean isNextFrame(int roll) {
+        return roll % 2 != 0;
+    }
+
+    public static boolean isFirstRollInFrame(int roll) {
+        return roll % 2 == 0;
+    }
+}
