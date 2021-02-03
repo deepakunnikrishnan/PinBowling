148ed65cdbedeb9f7ca240fad978dac6e24e1e9f
Deepak Unnikrishnan <deepak.unnikrishnan@ondotsystems.com>
2021-02-03 17:44:22 -0500

    Added CardView as wrapper for the ScoreboardView

--- app/src/main/java/com/androidnerds/bowling/game/components/scoreboard/ScoreboardView.java
+++ app/src/main/java/com/androidnerds/bowling/game/components/scoreboard/ScoreboardView.java
@@ -3,16 +3,20 @@ package com.androidnerds.bowling.game.components.scoreboard;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.androidnerds.bowling.R;
+import com.androidnerds.bowling.databinding.LayoutScoreboardBinding;
 import com.androidnerds.bowling.game.domain.GameEngine;
 import com.androidnerds.bowling.game.domain.model.Scoreboard;
 
-public class ScoreboardView extends RecyclerView implements GameEngine.OnScoreChangeListener {
+public class ScoreboardView extends CardView implements GameEngine.OnScoreChangeListener {
 
     private final Context context;
     private ScoreboardAdapter scoreboardAdapter;
@@ -24,21 +28,32 @@ public class ScoreboardView extends RecyclerView implements GameEngine.OnScoreCh
     public ScoreboardView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
-        initWithScoreboard();
+        init(context);
     }
 
-    private void init(@NonNull ScoreboardAdapter scoreboardAdapter) {
+    private void init(LayoutScoreboardBinding binding, @NonNull ScoreboardAdapter scoreboardAdapter) {
         if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            this.setLayoutManager(new GridLayoutManager(this.context, GameEngine.MAX_FRAMES/2));
+            binding.recyclerViewScoreboard.setLayoutManager(new GridLayoutManager(this.context, GameEngine.MAX_FRAMES/2));
         }else {
-            this.setLayoutManager(new GridLayoutManager(this.context, GameEngine.MAX_FRAMES));
+            binding.recyclerViewScoreboard.setLayoutManager(new GridLayoutManager(this.context, GameEngine.MAX_FRAMES));
         }
-        setAdapter(scoreboardAdapter);
+        binding.recyclerViewScoreboard.setAdapter(scoreboardAdapter);
     }
 
-    private void initWithScoreboard() {
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutScoreboardBinding binding = DataBindingUtil.inflate(inflater, R.layout.layout_scoreboard,this, true);
+        setCardViewStyles(context);
         scoreboardAdapter = new ScoreboardAdapter();
-        init(scoreboardAdapter);
+        init(binding, scoreboardAdapter);
+    }
+
+    private void setCardViewStyles(Context context) {
+        setRadius(context.getResources().getDimension(R.dimen.card_radius));
+        setCardElevation(context.getResources().getDimension(R.dimen.card_elevation));
+        int padding = (int) context.getResources().getDimension(R.dimen.default_padding);
+        setContentPadding(padding,padding,padding,padding);
     }
 
     private void updateScoreboard(ScoreboardAdapter adapter, Scoreboard scoreboard) {
